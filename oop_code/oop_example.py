70a9341208697dac071dd44f71b359ff8427738c
kryssyco <kjshepard70@hotmail.com>
2020-01-20 20:30:54 -0700

    OOP Coding example - TicTacToe Game

---
+++ oop_code/oop_example.py
@@ -0,0 +1,24 @@
+"""
+Classes to represent Games.
+"""
+
+class Game:
+
+    fun_level = 5
+
+    def __init__(self, player1='Alice', player2='Bob'):
+        self.rounds = 2
+        self.current_round = 0
+        self.player1 = player1
+        self.player2 = player2
+
+    def add_round(self):
+        self.rounds += 1
+
+    def print_players(self):
+        print("{} is playing {}".format(self.player1, self.player2))
+
+class TicTacToe(Game):
+    """TicTacToe Game representations"""
+    def print_players(self):
+        print('{} is playing Tic-Tac-Toe with {}'.format(self.player1, self.player2))
